be4cb13d2361aee0089ad300b5c76216542ac152
Adrian <adriangonzalesval@gmail.com>
2023-02-14 16:10:22 -0500

    fix: improved use of the ProxyPingEvent event
    Replaced unnecessary code for the use of the asBuilder() method that was used incorrectly
    
    The main class of each plugin is already registered as a listener by Velocity at plugin startup, so I removed the unnecessary use of onProxyInitialization

--- src/main/java/me/xii69/justonemore/JustOneMore.java
+++ src/main/java/me/xii69/justonemore/JustOneMore.java
@@ -1,13 +1,9 @@
 package me.xii69.justonemore;
 
-import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.ServerPing;
-import org.slf4j.Logger;
 
 @Plugin(
         id = "justonemore",
@@ -19,35 +15,13 @@ import org.slf4j.Logger;
 
 public class JustOneMore {
 
-    private final ProxyServer server;
-    private final Logger logger;
-
-    @Inject
-    public JustOneMore(ProxyServer server, Logger logger) {
-        this.server = server;
-        this.logger = logger;
-    }
-
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        server.getEventManager().register(this, this);
-    }
-
     @Subscribe
     public void onProxyPing(ProxyPingEvent event) {
-        ServerPing serverPing = event.getPing();
+        final ServerPing serverPing = event.getPing();
         if (serverPing.getPlayers().isPresent()) {
-            int onlinePlayers = serverPing.getPlayers().get().getOnline();
-            ServerPing.Players pingPlayers = new ServerPing.Players(onlinePlayers, onlinePlayers + 1, serverPing.asBuilder().getSamplePlayers());
-            event.setPing(
-                    new ServerPing(
-                            serverPing.getVersion(),
-                            pingPlayers,
-                            serverPing.getDescriptionComponent(),
-                            serverPing.getFavicon().orElse(null)
-                    )
-            );
+            final ServerPing.Builder builder = serverPing.asBuilder();
+            final int onlinePlayers = builder.getOnlinePlayers();
+            event.setPing(builder.maximumPlayers(onlinePlayers + 1).build());
         }
     }
-
 }
