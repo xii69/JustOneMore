2f0b773739cab671210915c2a95af0f6bee1444f
xii69 <87283583+xii69@users.noreply.github.com>
2024-10-18 16:23:20 +0330

    Little optimizations

--- src/main/java/me/xii69/justonemore/JustOneMore.kt
+++ src/main/java/me/xii69/justonemore/JustOneMore.kt
@@ -7,18 +7,14 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.event.proxy.ProxyPingEvent
 import me.xii69.justonemore.Metrics.Factory
 
-
 class JustOneMore @Inject constructor(
     private val metricsFactory: Factory
 ) {
     @Subscribe
-    fun onProxyInitialization(event: ProxyInitializeEvent) {
-        metricsFactory.make(this, 22308)
-    }
+    fun onProxyInitialization(event: ProxyInitializeEvent) = metricsFactory.make(this, 22308)
 
     @Subscribe(order = PostOrder.LAST)
     fun onProxyPing(event: ProxyPingEvent) {
-        if (!event.ping.players.isPresent) return
-        event.ping = event.ping.asBuilder().maximumPlayers(event.ping.asBuilder().onlinePlayers + 1).build();
+        if (event.ping.players.isPresent) event.ping = event.ping.asBuilder().maximumPlayers(event.ping.asBuilder().onlinePlayers + 1).build()
     }
 }
